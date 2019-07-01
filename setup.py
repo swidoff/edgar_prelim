600d787ba0e53caf09214e0a88e46390e23f89a6
Seth <swidoff@gmail.com>
2019-07-01 13:39:15 -0700

    Add setup.py.

---
+++ setup.py
@@ -0,0 +1,12 @@
+# setup.py
+from distutils.core import setup
+
+setup(
+    name='edgar_prelim',
+    version='0.0.1',
+    author='Seth Widoff',
+    author_email='swidoff@gmail.com',
+    url='https://github.com/swidoff/edgar_prelim',
+    packages=['edgar_prelim', 'edgar_prelim.resources'],
+)
+
